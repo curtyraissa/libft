0b62d0a9766f516f1bbc83b2e2e9fe5f22440e50
curtyraissa <raissacurty@gmail.com>
2024-10-17 13:55:35 +0100

    feat: add strdup function

---
+++ ft_strdup.c
@@ -0,0 +1,27 @@
+#include "libft.h"
+
+char *ft_strdup(const char *s1)
+{
+	char	*dup;
+	size_t	len;
+	size_t	i;
+
+	len = ft_strlen(s1);
+	dup = (char *)malloc(len + 1);
+	if (!dup)
+		return (NULL);
+	while (i < len)
+	{
+		dup[i] = s1[i];
+		i++;
+	}
+	dup[len] = '\0';
+	return (dup);
+}
+
+#include <stdio.h>
+int	main(void)
+{
+	printf("%s\n", ft_strdup("Hello, 42!"));
+	return (0);
+}
