c256cfb9e84e1eb60c98a5674b17d092b112b4bf
curtyraissa <raissacurty@gmail.com>
2024-10-17 13:55:09 +0100

    feat: add atoi function

---
+++ ft_atoi.c
@@ -0,0 +1,31 @@
+int	ft_atoi(const char *str)
+{
+	int	sign;
+	int	result;
+
+	sign = 1;
+	result = 0;
+	while (*str == ' ' || (*str >= '\t' && *str <= '\r'))
+		str++;
+	if (*str == '-' || *str == '+')
+	{
+		if (*str == '-')
+			sign = -1;
+		str++;
+	}
+	while (*str >= '0' && *str <= '9')
+	{
+		result = result * 10 + (*str - '0');
+		str++;
+	}
+	return (result * sign);
+}
+
+/*
+#include <stdio.h>
+int	main(void)
+{
+	printf("%d\n", ft_atoi("   -42"));
+	return (0);
+}
+*/
