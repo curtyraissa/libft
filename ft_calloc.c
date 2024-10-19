41b9e5d22301ed01417b87d27896a6e6b01ccd9c
curtyraissa <raissacurty@gmail.com>
2024-10-19 15:58:44 +0100

    feat: add calloc function

--- ft_calloc.c
+++ ft_calloc.c
@@ -10,4 +10,44 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-void *ft_calloc(size_t nmemb, size_t size);
+#include "libft.h"
+
+void *ft_calloc(size_t nmemb, size_t size)
+{
+    void *ptr;
+    size_t total_size;
+    size_t i;
+    
+    total_size = nmemb * size;
+    if (nmemb != 0 && total_size / nmemb != size)
+        return (NULL);
+    ptr = malloc(total_size);
+    if (!ptr)
+        return (NULL);
+    i = 0;
+    while (i < total_size)
+    {
+        ((unsigned char *)ptr)[i] = 0;
+        i++;
+    }
+    return (ptr);
+}
+
+/*
+#include <stdio.h>
+int main(void)
+{
+    int *arr;
+    size_t nmemb = 3;
+    arr = (int *)ft_calloc(nmemb, sizeof(int));
+    if (!arr)
+    {
+        printf("Memory allocation failed.\n");
+        return (1);
+    }
+    for (size_t i = 0; i < nmemb; i++)
+        printf("%d ", arr[i]);
+    free(arr);
+    return (0);
+}
+*/
