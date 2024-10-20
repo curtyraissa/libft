f7b06c29a667d81d4fa78a73ceb3d10963139fa6
curtyraissa <raissacurty@gmail.com>
2024-10-20 20:49:11 +0100

    feat: add memmove function

--- ft_memmove.c
+++ ft_memmove.c
@@ -10,4 +10,45 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-void *ft_memmove(void *dst, const void *src, size_t len);
+#include "libft.h"
+
+void *ft_memmove(void *dst, const void *src, size_t len)
+{
+	unsigned char *d;
+	const unsigned char *s;
+
+	d = (unsigned char *)dst;
+	s = (const unsigned char *)src;
+	if (d > s && d < s + len)
+	{
+		while (len--)
+		{
+			d[len] = s[len];  // Copia o último byte primeiro
+		}
+	}
+	else
+	{
+		while (len--)
+		{
+			*d++ = *s++;
+		}
+	}
+	return (dst);
+}
+
+/*
+#include <stdio.h>
+int main(void)
+{
+    char src[] = "Hello, world!";
+    char dst[20];
+
+    // Copiar sem sobreposição
+    ft_memmove(dst, src, 13);
+    printf("Destination after memmove (no overlap): %s\n", dst);
+    // Copiar com sobreposição
+    ft_memmove(src + 6, src, 6);
+    printf("Source after memmove (with overlap): %s\n", src);
+    return (0);
+}
+*/
