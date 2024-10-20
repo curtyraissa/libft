e4b39a6c9c531b74fe09db94109a6f8331f160f0
curtyraissa <raissacurty@gmail.com>
2024-10-20 18:54:46 +0100

    feat: add memset function

--- ft_memset.c
+++ ft_memset.c
@@ -10,4 +10,31 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-void *ft_memset(void *b, int c, size_t len);
+#include "libft.h"
+
+void *ft_memset(void *b, int c, size_t len)
+{
+	unsigned char *ptr;
+	size_t i;
+
+	ptr = (unsigned char *)b;
+	i = 0;
+	while (i < len)
+	{
+		ptr[i] = (unsigned char)c;
+		i++;
+	}
+	return (b);
+}
+
+/*
+#include <stdio.h>
+#include <string.h>
+int main(void)
+{
+    char str[50] = "Hello, world!";
+    memset(str, 'A', 5);
+    printf("%s\n", str);
+    return (0);
+}
+*/
