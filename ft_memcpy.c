5f9159d705efedb6098baaa6870235e53735d6ca
Raissa Curty <117386154+curtyraissa@users.noreply.github.com>
2024-10-21 12:35:04 +0100

    fix: norminette ok

--- ft_memcpy.c
+++ ft_memcpy.c
@@ -6,21 +6,21 @@
 /*   By: rcurty-g <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/18 16:49:30 by rcurty-g          #+#    #+#             */
-/*   Updated: 2024/10/18 16:49:32 by rcurty-g         ###   ########.fr       */
+/*   Updated: 2024/10/21 12:30:52 by rcurty-g         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void *ft_memcpy(void *dst, const void *src, size_t n)
+void	*ft_memcpy(void *dst, const void *src, size_t n)
 {
-	size_t i;
+	size_t	i;
 
 	i = 0;
 	while (i < n)
 	{
 		((unsigned char *)dst)[i] = ((unsigned char *)src)[i];
- 		i++;
+		i++;
 	}
 	return (dst);
 }
@@ -30,7 +30,7 @@ void *ft_memcpy(void *dst, const void *src, size_t n)
 int main(void)
 {
     char dst[20];
-    
+
 	ft_memcpy(dst, "Hello, world!", 13);
     printf("Destination after memcpy: %s\n", dst);
 	return (0);
