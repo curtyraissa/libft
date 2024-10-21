16e030fec397e18ea4144feaf117dcfb925ce2fc
Raissa Curty <117386154+curtyraissa@users.noreply.github.com>
2024-10-21 12:34:48 +0100

    fix: norminette ok

--- ft_memcmp.c
+++ ft_memcmp.c
@@ -6,21 +6,20 @@
 /*   By: rcurty-g <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/18 16:49:14 by rcurty-g          #+#    #+#             */
-/*   Updated: 2024/10/18 16:49:18 by rcurty-g         ###   ########.fr       */
+/*   Updated: 2024/10/21 12:29:49 by rcurty-g         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-int ft_memcmp(const void *s1, const void *s2, size_t n)
+int	ft_memcmp(const void *s1, const void *s2, size_t n)
 {
-	size_t i;
-	unsigned char *p1;
-	unsigned char *p2;
+	size_t			i;
+	unsigned char	*p1;
+	unsigned char	*p2;
 
 	p1 = (unsigned char *)s1;
 	p2 = (unsigned char *)s2;
-
 	i = 0;
 	while (i < n)
 	{
