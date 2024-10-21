9bb3268dadf1d9ad2d7fab5f67285efd37fdbd0c
Raissa Curty <117386154+curtyraissa@users.noreply.github.com>
2024-10-21 14:16:51 +0100

    feat: add substr function

---
+++ ft_substr.c
@@ -0,0 +1,59 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_substr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rcurty-g <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/21 12:37:44 by rcurty-g          #+#    #+#             */
+/*   Updated: 2024/10/21 14:15:19 by rcurty-g         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_substr(char const *s, unsigned int start, size_t len)
+{
+	char	*substr;
+	size_t	i;
+	size_t	s_len;
+
+	if (!s)
+		return (NULL);
+	s_len = 0;
+	while (s[s_len])
+		s_len++;
+	if (start >= s_len)
+	{
+		substr = (char *)malloc(1);
+		if (!substr)
+			return (NULL);
+		substr[0] = '\0';
+		return (substr);
+	}
+	substr = (char *)malloc((len + 1) * sizeof(char));
+	if (!substr)
+		return (NULL);
+	i = 0;
+	while (s[start] && i < len)
+		substr[i++] = s[start++];
+	substr[i] = '\0';
+	return (substr);
+}
+
+/*
+#include <stdio.h>
+int main(void)
+{
+    char *s = "Hello, world!";
+    char *sub;
+
+    sub = ft_substr(s, 7, 5);
+    if (sub)
+    {
+        printf("Substring: %s\n", sub);
+        free(sub);
+    }
+    return (0);
+}
+*/
