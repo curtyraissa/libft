0cda3cf7a229ac5733a15d0317d15a5a83514e89
Raissa Curty <117386154+curtyraissa@users.noreply.github.com>
2024-10-22 17:21:59 +0100

    add: feat striteri function

--- ft_striteri.c
+++ ft_striteri.c
@@ -6,8 +6,40 @@
 /*   By: rcurty-g <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/22 15:43:05 by rcurty-g          #+#    #+#             */
-/*   Updated: 2024/10/22 15:43:07 by rcurty-g         ###   ########.fr       */
+/*   Updated: 2024/10/22 17:21:26 by rcurty-g         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-void ft_striteri(char *s, void (*f)(unsigned int, char*));
+#include "libft.h"
+
+void	ft_striteri(char *s, void (*f)(unsigned int, char *))
+{
+	unsigned int	i;
+
+	if (!s || !f)
+		return ;
+	i = 0;
+	while (s[i])
+	{
+		f(i, &s[i]);
+		i++;
+	}
+}
+
+/*
+#include <stdio.h>
+void	to_uppercase(unsigned int i, char *c)
+{
+	(void)i;
+	if (*c >= 'a' && *c <= 'z')
+		*c = *c - 32;
+}
+int	main(void)
+{
+	char	str[] = "hello, world!";
+
+	ft_striteri(str, to_uppercase);
+	printf("Resultado: %s\n", str);
+	return (0);
+}
+*/
